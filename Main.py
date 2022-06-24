fb80dd78c72e412ddec6886b2d676c6d17796a0b
Sandesh Jangir <sandeshatgithub@gmail.com>
2022-06-24 13:20:27 +0530

    Added The Fetch Ability and Increadsed code's Readability

--- Main.py
+++ Main.py
@@ -1,16 +1,32 @@
-from flask import Flask, jsonify
+from flask import Flask,jsonify
+import json
+import requests
 
 app = Flask(__name__)
 
-@app.route("/")
-def Py_Api():
-    Data = {
+    
+@app.route("/api")
+def api_route():
+    data = {
         "Author":"Sandesh",
         "Editor":"Visual Studio Code",
         "Email":"noreply@example.com"
     }
-    Api = jsonify(Data)
-    return Api
+    # Jsonifying Dict Type "data" to JSON format
+    jsonified_data = jsonify(data)
+    return jsonified_data
+
+@app.route("/")
+def home_route():
+    # Fetching Api
+    api_url = requests.get("http://localhost:80/api").text
+    # Converting API Data to Python Dict Type
+    dict_api = json.loads(api_url)
+
+    # Creating a custom message to show using API Data
+    str_to_display = f'Author is {dict_api["Author"]} and He uses {dict_api["Editor"]} to write his code and his e-mail is {dict_api["Email"]}'
+
+    return str_to_display
 
 if __name__ == "__main__":
     app.run(debug=True, port=80)
